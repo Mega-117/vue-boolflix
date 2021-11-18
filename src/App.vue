50a858138256d6998a231202d3ed5480e9fe37ce
Mega-117 <enricosaian@hotmail.it>
2021-11-18 09:25:13 +0100

    correzioni

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="parolaDaCercare" @keyup.enter="ricercaMovie" type="text" />
+    <button @click="ricercaMovie">cerca</button>
+    <ul>
+      <li v-for="elemento in arrRispostaApiMovie" :key="elemento.id">
+        titolo{{ elemento.title }} <br />
+        titolo originale{{ elemento.original_title }} <br />
+        lingua {{ elemento.original_language }} <br />
+        voto {{ elemento.vote_average }}
+        <hr />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      urlApiBase: "https://api.themoviedb.org/3",
+      chiaveApi: "61cc31731d807d15a32cde73a023a2b5",
+      arrRispostaApiMovie: [],
+      parolaDaCercare: "",
+
+      bandiere: {
+        it: "FlagIt.png",
+        en: "./assets/FlagUk.png",
+        de: "./assets/FlagDe.png",
+        es: "./assets/FlagEs.png",
+        jp: "./assets/FlagJp.png",
+      },
+    };
+  },
+  methods: {
+    ricercaMovie() {
+      this.chiamataApi(
+        "/search/movie",
+        this.parolaDaCercare,
+        "arrRispostaApiMovie"
+      );
+    },
+
+    chiamataApi(urlRichiestaApi, queryDaCercare, arrDovePushare) {
+      //axios.get(urlapi che è omposto da [urlbaseapi] + [il comando per la richiesta ] + [la chiave api] + [e la query])
+      /*  versione senza param
+          axios.get(this.urlApiBase + "/search/movie" + "?api_key=" + this.chiaveApi + "&query=ciao"
+          ); */
+      axios
+        .get(this.urlApiBase + urlRichiestaApi, {
+          params: {
+            api_key: this.chiaveApi,
+            query: queryDaCercare,
+          },
+        })
+        .then((respose) => {
+          debugger;
+          this[arrDovePushare] = respose.data.results;
+        });
+    },
+  },
+  mounted() {},
+};
 </script>
 
 <style lang="scss">
+@import "@/styles/app.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
